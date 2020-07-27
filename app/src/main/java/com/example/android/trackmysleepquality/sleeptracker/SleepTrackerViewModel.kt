6dd6e0e0df7a29b83caac5c3f31b9e28966377bd
Maja Francetic <francetic.maja@gmail.com>
2020-07-27 18:01:40 +0200

    Add methods initializeTonight, onCleared, getTonightFromDatabase, onStartTracking, insert, onStopTracking, update, onClear and clar in the SleepTrackerViewModel; add the methods as onClick in the .xml file

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -17,8 +17,16 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
+import android.os.strictmode.ServiceConnectionLeakedViolation
+import android.provider.SyncStateContract.Helpers.insert
+import android.provider.SyncStateContract.Helpers.update
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -28,5 +36,104 @@ class SleepTrackerViewModel(
         // we need Application access, to access Strings and other resources
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
-}
 
+    private var viewModelJob = Job()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    // will run on the main thread (sensible for the ViewModel)
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    private var nights = database.getAllNights()
+
+    val nightsString= Transformations.map(nights) {
+        nights -> formatNights(nights, application.resources)
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        // we use coroutines, so we do not block the UI while waiting for the result
+        // launch = without blocking the UI thread
+        // executes immediately
+        uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    /**
+     *  Handling the case of the stopped app or forgotten recording,
+     *  the start and end times will be the same.j
+     *
+     *  If the start time and end time are not the same, then we do not have an unfinished
+     *  recording.
+     */
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        return withContext(Dispatchers.IO) {
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    /**
+     * Executes when the START button is clicked.
+     */
+    fun onStartTracking() {
+        // we use a coroutine, because everything will be time-consuming
+        // UIscope - because we need it to update the UI with the result
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        return withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    /**
+     * Executes when the STOP button is clicked.
+     */
+    fun onStopTracking() {
+        uiScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        return withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    /**
+     * Executes when the CLEAR button is clicked.
+     */
+    private suspend fun clear(){
+        return withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+}
