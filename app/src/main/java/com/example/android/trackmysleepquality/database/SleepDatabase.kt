4ddc18abd018b367c94b37caa8da6645967fdf87
Maja Francetic <francetic.maja@gmail.com>
2020-07-27 16:25:20 +0200

    Create an abstract class SleepDatabase, with a companion object and its method getInstance, that returns the instance of the database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,54 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// add all the tables in the list (we only have SleepNight database)
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    // we can have multiple DAO's
+    abstract val sleepDatabaseDao: SleepDatabaseDao;
+
+    // we do not need to instantiate the class, so we have a companion object
+    companion object {
+
+        // @Volatile = it makes sure the value of INSTANCE is always up to date and
+        // the same to all execution threads
+        // the value will never be cached, and all writes and reads will be done to and from the main memory
+        @Volatile
+        // this will avoid us repeatedly opening connections to the database, which is expensive
+
+        private var INSTANCE: SleepDatabase? = null
+
+        // returns the reference to the SleepDatabase
+        fun getInstance(context: Context): SleepDatabase {
+            // synchronized = means only one thread of execution at a time can enter this
+            // block of code
+            synchronized(this) {
+                // smart cast - available only to local variables (not class variables)
+                var instance = INSTANCE;
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            // normally, we need to provide a migration object with a migration
+                            // strategy when we create the database
+                            // MIGRATION STRATEGY = tells us how to take all the rows of the old
+                            // schema and convert them to rows in the new schema
+                            .fallbackToDestructiveMigration()
+                            .build();
+                    INSTANCE = instance
+                }
+                return instance;
+            }
+        }
+    }
+}
